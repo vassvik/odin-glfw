07f40d29e34e059e77019b7b6d1aa96418e88553
vassvik <mvassvik@gmail.com>
2020-10-09 21:35:07 +0200

    Fix build_glfw.bat to work correctly with CMake 3.15+

--- build_glfw.bat
+++ build_glfw.bat
@@ -13,7 +13,7 @@ pushd dependencies
 	pushd glfw
 	    mkdir build 
 	    pushd build 
-	        cmake -G "NMake Makefiles" .. -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF -DUSE_MSVC_RUNTIME_LIBRARY_DLL=OFF -DCMAKE_BUILD_TYPE=RELEASE
+	        cmake -G "NMake Makefiles" .. -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded -DCMAKE_BUILD_TYPE=RELEASE
 	    popd
 	    cmake --build build --config Release
 	    copy build\src\glfw3.lib ..\..\lib\glfw3.lib
